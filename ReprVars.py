c138d9a52a1df6aeaf99671502c20a8224f518df
Tu Nguyen <tnguy177@jhu.edu>
2018-03-14 00:40:16 -0400

    Fixed bugs on Tu's code

--- ReprVars.py
+++ ReprVars.py
@@ -6,6 +6,8 @@ import scipy.sparse.linalg          # To perform linear algebra operation on spa
 import copy
 import io as  cStringIO
 import re
+from scipy.stats import variation
+import math
 
 
 # Making changes in Sri branch
@@ -384,7 +386,7 @@ class MIP:
             'Slack_1_max'    :   np.max(slack1),
             'Slack_1_25p'    :   np.percentile(slack1, 25),
             'Slack_1_75p'    :   np.percentile(slack1, 75),
-            'Slack_1_norm'    :   np.linalg.norm(slack1),
+            'Slack_1_norm'   :   np.linalg.norm(slack1),
             'Slack_2_mean'   :   np.mean(slack2),
             'Slack_2_std'    :   np.std(slack2),
             'Slack_2_min'    :   np.min(slack2),
@@ -394,6 +396,170 @@ class MIP:
             'Slack_2_norm'    :   np.linalg.norm(slack2),
             "Slack_1_nonzero":   np.sum(abs(slack1)>=tol)
         }
+
+    def stdObjM(self):
+	    """
+	    Standard deviation of normalized coefficients: f_i/Number_of_constraints
+	    """
+	    stdObjM= np.std(self.f/self.Aeq.shape[0],ddof=1)
+	    return {"stdObjM": stdObjM}
+  
+    def stdObjN(self):
+    	"""
+    	#Standard deviation of ci/ni where ni denotes 
+	#the number of nonzero entries in column i of A
+	"""
+    	n = self.Aeq.shape[1]
+    	m = self.Aeq.shape[0]
+    	fNew1 = np.zeros((1,n))
+    	fNew2 = np.zeros((1,n))
+    	for i in range(n):
+            c = 0
+            for j in range(m):
+                if self.Aeq[j][i] != 0:
+                    c += 1
+            fNew1[0][i] = self.f[i]/c
+            fNew2[0][i] = self.f[i]/np.sqrt(c)
+    	stdObjN=np.std(fNew1,ddof=1)
+    	stdObjRootN = np.std(fNew2,ddof=1)
+    	return {'stdObjN':stdObjN, 'stdObjRootN':stdObjRootN}
+
+    def AeqNormStats(self):
+        """
+        Distribution of normalized constraint matrix entries, 
+        Aij/bi: mean and std (only of elements where bi != 0)
+        """
+    
+        n = self.Aeq.shape[1]
+        m = self.Aeq.shape[0]
+        AeqNorm=np.zeros((m,n))
+        for i in range(m):
+    	    for j in range(n):
+                if self.beq[i]!=0:
+                    AeqNorm[i][j]=self.Aeq[i][j]/self.beq[i]
+        AeqNormMean=np.mean(AeqNorm)
+        AeqNormStd=np.std(AeqNorm,ddof=1)
+        return {'AeqNormMean':AeqNormMean,'AeqNormStd':AeqNormStd}
+
+    def CVStats(self):
+        """
+        Variation coefficient of normalized absolute nonzero 
+        entries per row: mean and Std
+        """
+        AeqAbsolute=np.absolute(self.Aeq)
+        var=variation(AeqAbsolute,axis=1)
+        CVMean=np.mean(var)
+        CVStd=np.std(var,ddof=1)
+        return {'CVMean':CVMean,'CVStd':CVStd}
+
+    def AOverb(self):
+        """
+        Min/max for ratios of constraint coeffs. to RHS: Min and Max 
+        ratios across positive and negative right-hand-sides 
+        """
+        n = self.Aeq.shape[1]
+        m = self.Aeq.shape[0]
+        b=np.zeros((1,n))
+
+        MinPos = math.inf
+        MaxPos = -math.inf
+        MinNeg = math.inf
+        MaxNeg = -math.inf
+
+        for i in range(m):
+            if self.beq[i]>0:
+                for j in range(n):
+                    b[0][j]=self.Aeq[i][j]/self.beq[i]
+                    MinTemp = np.amin(b)
+                    MaxTemp = np.amax(b)
+                if MinTemp<MinPos:
+                    MinPos = MinTemp
+                if MaxTemp>MaxPos:
+                    MaxPos = MaxTemp
+            elif self.beq[i]<0:
+                for j in range(n):
+                    b[0][j]=self.Aeq[i][j]/self.beq[i]
+                    MinTemp1 = np.amin(b)
+                    MaxTemp1 = np.amax(b)
+                if MinTemp1<MinNeg:
+                    MinNeg = MinTemp1
+                if MaxTemp1>MaxNeg:
+                    MaxNeg = MaxTemp1
+        return {'MinPos':MinPos,'MaxPos':MaxPos,'MinNeg':MinNeg,'MaxNeg':MaxNeg}
+	  
+    def OnetoAllA(self):
+        """  
+        Min/max for one-to-all coeff ratios: The statistics are over the 
+        ratios of a variables coefficient, to the sum over all other variables 
+        coefficients, for a given constraint. Four versions of these ratios are 
+        considered: positive (negative) coefficient to sum of positive (negative) 
+        coefficients
+        """
+        n = self.Aeq.shape[1]
+        m = self.Aeq.shape[0]
+	  
+        MinPosPos = math.inf
+        MaxPosPos = -math.inf
+        MinPosNeg = math.inf
+        MaxPosNeg = -math.inf
+        MinNegPos = math.inf
+        MaxNegPos = -math.inf
+        MinNegNeg = math.inf
+        MaxNegNeg = -math.inf
+
+        for i in range(m):
+            a=self.Aeq[i][:]
+            pos=a[a>0]
+            neg=a[a<0]
+            sumPos=np.sum(pos)
+            sumNeg=np.sum(neg)
+            sizeP = np.size(pos)
+            sizeN = np.size(neg)
+            c=np.zeros((1,sizeP))
+            d=np.zeros((1,sizeP))
+            e=np.zeros((1,sizeN))
+            f=np.zeros((1,sizeN))
+            for j in range(sizeP):
+                if (sumPos-pos[j])==0:
+                    c[0][j]=pos[j]
+                else:
+                    c[0][j]=pos[j]/(sumPos-pos[j]) #positive/positive
+                d[0][j]=pos[j]/sumNeg #positive/negative
+                MinPosTemp=np.amin(c)
+                MaxPosTemp=np.amax(c)
+                MinNegTemp=np.amin(d)
+                MaxNegTemp=np.amax(d)
+            if MinPosTemp<MinPosPos:
+                MinPosPos = MinPosTemp
+            if MaxPosTemp>MaxPosPos:
+                MaxPosPos = MaxPosTemp
+            if MinNegTemp<MinPosNeg:
+                MinPosNeg = MinNegTemp
+            if MaxNegTemp>MaxPosNeg:
+                MaxPosNeg = MaxNegTemp
+            for j in range(sizeN):
+                if (sumNeg-neg[j])==0:
+                    e[0][j]=neg[j]
+                else:
+                    e[0][j]=neg[j]/(sumNeg-neg[j]) #negative/negative
+                f[0][j]=neg[j]/sumPos #negative/positive
+                MinPosTemp=np.amin(f)
+                MaxPosTemp=np.amax(f)
+                MinNegTemp=np.amin(e)
+                MaxNegTemp=np.amax(e)
+            if MinPosTemp<MinNegPos:
+                MinNegPos = MinNegTemp
+            if MaxPosTemp>MaxNegPos:
+                MaxNegPos = MaxPosTemp
+            if MinNegTemp<MinNegNeg:
+                MinNegNeg = MinNegTemp
+            if MaxNegTemp>MaxNegNeg:
+                MaxNegNeg = MaxNegTemp
+        return{'MinPosPos':MinPosPos,'MaxPosPos':MaxPosPos,'MinPosNeg':MinPosNeg,
+    		'MaxPosNeg':MaxPosNeg,'MinNegPos':MinNegPos,
+    		'MaxNegPos':MaxNegPos,'MinNegNeg':MinNegNeg,
+    		'MaxNegNeg':MaxNegNeg}
+		
     def VGraph(self, tol=1e-9):
         """
         Returns statistics of the variable graph. It is a simple graph where each vertex
